1457fd9c198f92e366672f7755779f9e4a7a8479
shill Bastian Dongmo <shillbastian@gmail.com>
2025-06-17 17:43:40 -0400

    taking the user disk type

--- free_memory.py
+++ free_memory.py
@@ -2,7 +2,10 @@
 
 def main():
 	#should throw succees if works
+
 	print('hey the  memory is not saturated !')
+	a= input('enter the memory disk type')
+	print('{} is a free disk from now'.format(a))
 	return True
 
 main()
